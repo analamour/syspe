165b05c572498d3fd0c639883076fb1fd744e7f3
Amy <amirakollrich@gmail.com>
2023-10-04 01:07:42 -0300

    modificado el ModelUser

---
+++ models/ModelUser.py
@@ -0,0 +1,18 @@
+from.entities.User import User
+
+class ModelUser():
+    @classmethod
+    def login(self, db, user):
+        try:
+            cur=db.connection.curson()
+            sql=f"""SELECT id, username, password, fullname FROM usuarios 
+            WHERE username = {user.username}"""
+            cur.execute(sql)
+            row=cur.fetchone()
+            if row != None:
+                user=User(row[0], row[1],User.check_password(row[2],user.password), row[3])
+                return user
+            else:
+                None
+        except Exception as ex:
+            raise Exception(ex)
